eaf4f14ec8405c2cef655b6763c20ea60aabe127
Thomas Orlans <thomasorlans@Thomass-MacBook-Pro.local>
2023-04-24 18:28:04 +0200

    change browserRouter on HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import Realisations from "./components/pages/Realisations";
@@ -7,14 +7,14 @@ import Contact from "./components/pages/Contact";
 function App() {
   return (
     <div>
-      <Router>
+      <HashRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/services" element={<Services />} />
           <Route exact path="/realisations" element={<Realisations />} />
           <Route exact path="/contact" element={<Contact />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
